1b9fb2e3a7cd60c6179a067d2c9cfca18b6e3d67
christoffee <chris.s.dennis@gmail.com>
2016-05-29 22:07:53 +0100

    Updates

--- gulp/tasks/build.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-gulp.task('build', ['browserify', 'markup']);
-gulp.task('copy', function() {
-    return gulp.src(['images/**/*'])
-        .pipe(gulp.dest('build/images'));
-});
